a4bd766662afcc6c94210701e5a08dad16c1973e
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-02-04 12:23:33 -0800

    GumballMachine Type 1 Implementation
    Machine 1 accepts only quarters. Gives an error message if coin is not a quarter / Insufficient gumballs available

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,41 +3,98 @@ public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    //private boolean has_quarter;
+    private int value_inserted;
+    private int gumball_cost;
+    private int machine_type;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int type )
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        //this.has_quarter = false;
+        this.value_inserted = 0;
+        switch(type)
+        {
+            case 1: gumball_cost = 25; machine_type = 1; break;
+            case 2: gumball_cost = 50; machine_type = 2; break;
+            case 3: gumball_cost = 50; machine_type = 3; break;
+            default : System.out.println("Please enter a valid machine type (1,2 or 3)");
+        }
+
     }
 
-    public void insertQuarter(int coin)
+    public void addGumballs(int size)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        if(size>0)
+        {
+            this.num_gumballs +=size;
+            System.out.println("Added "+size+" gumballs. Now the size is = "+this.num_gumballs);
+        }
+        else
+        {
+            System.out.println("Please enter a positive number");
+        }
     }
     
+    public void insertCoin(int coin)
+    {
+        if ( machine_type == 3 && (isQuarter(coin) || isNickel(coin) || isDime(coin) ) )
+        {
+            this.value_inserted+=coin;
+        }
+        else if(machine_type == 1 && isQuarter(coin) )
+        {
+            this.value_inserted+=coin;
+        }
+        else if(machine_type == 2 && isQuarter(coin) )
+        {
+            this.value_inserted+=coin;
+        }
+        else 
+            System.out.println("Coin of value "+coin+" is invalid for machine type "+machine_type);         
+    }
+
+    private boolean isQuarter(int coin)
+    {
+        return coin == 25;   
+    }
+
+    private boolean isNickel(int coin)
+    {
+        return coin == 5;   
+    }
+
+    private boolean isDime(int coin)
+    {
+        return coin == 10;   
+    }
+
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if(this.num_gumballs > 0 && this.value_inserted >= gumball_cost)
+        {
+            switch(machine_type)
+            {
+                case 1: System.out.println("For Gumball Machine Type "+machine_type);
+                        System.out.println("Value inserted "+value_inserted);
+                        this.num_gumballs--;
+                        this.value_inserted = this.value_inserted - this.gumball_cost;
+                        System.out.println("Value remaining "+value_inserted);
+                        System.out.println("Quarter received. Gumball ejected");
+                        if(this.value_inserted > 0)
+                        {
+                            System.out.println("Ejecting change - "+this.value_inserted);
+                            this.value_inserted = 0;
+                            System.out.println("Gumballs remaining "+this.num_gumballs);
+                        }
+                        break;
+            }
+        }
+        else if (this.num_gumballs == 0)
+            System.out.println("No gumballs remaining. Be right back");
+        else if (this.value_inserted < gumball_cost)
+            System.out.println("Falling short. Please insert "+(gumball_cost - value_inserted)+" cents more");
+     
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -2,21 +2,25 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        
+        //Testing for Gumball Machine of type 1
+        
+        GumballMachine gumballMachine = new GumballMachine(1,1);
+  
+        gumballMachine.insertCoin( 25 ); 
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.turnCrank(); //Successfully ejected with change returned
+        
+        gumballMachine.insertCoin(10); //Invalid Coin Type
+        
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.turnCrank(); // Insufficient gumballs
+        
+        gumballMachine.addGumballs(5);
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+        //System.out.println(gumballMachine);
+    }
 }
