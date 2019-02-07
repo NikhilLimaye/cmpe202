8a731abf2c11d6676f7c63581f278a2bdcf0e07b
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-02-07 10:24:01 -0800

    Corrected a few errors in the astah file.

--- lab1/gumball-java-pattern/Main.java
+++ lab1/gumball-java-pattern/Main.java
@@ -3,13 +3,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5,1);
+		
 
 		//System.out.println(gumballMachine);
 		
 		System.out.println("****Testing for gumball machine type 1****");
-
+		GumballMachine gumballMachine = new GumballMachine(5,1);
 		gumballMachine.insertCoin(25);
+		gumballMachine.turnCrank();
 		gumballMachine.insertCoin(25);
 		gumballMachine.turnCrank();
 		gumballMachine.insertCoin(4);
@@ -24,18 +25,13 @@ public class Main {
 		gumballMachine2.turnCrank();
 
 		
-
 		System.out.println("****Testing for gumball machine type 3****");
 		GumballMachine gumballMachine3 = new GumballMachine(5,3);
 		gumballMachine3.insertCoin(10);
 		gumballMachine3.insertCoin(25);
 		gumballMachine3.turnCrank();
-
 		gumballMachine3.insertCoin(25);
 		gumballMachine3.turnCrank();
-
-
-
 		//System.out.println(gumballMachine);
 	}
 }
