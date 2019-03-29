db727b58e863a21a634e5ecd087740e6322b50db
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-03-29 14:54:38 -0700

    Added Decorator Class

---
+++ lab8/inputmask/src/main/java/Decorator.java
@@ -0,0 +1,16 @@
+
+public class Decorator implements Component {
+		
+	public String addSeparator() {		
+		
+		return " ";
+		
+	}
+
+	public String addDoubleSpace() {
+		
+		return "  ";
+		
+	}
+
+}
