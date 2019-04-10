13216d0cc12ea2dc09a39765d7b818aa93108459
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-04-10 16:01:50 -0700

    Update Main.java

--- lab9/StretegyPatternUsingFunctionalProgramming/Main.java
+++ lab9/StretegyPatternUsingFunctionalProgramming/Main.java
@@ -9,8 +9,13 @@ public class Main {
 		
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
 		
+		System.out.println("Adding all numbers");
 		System.out.println(totalValues(numbers , e -> true));
+		
+		System.out.println("Adding all even numbers");
 		System.out.println(totalValues(numbers , e -> e%2==0));
+		
+		System.out.println("Adding all odd numbers");
 		System.out.println(totalValues(numbers , e -> e%2!=0));
 	
 	}
